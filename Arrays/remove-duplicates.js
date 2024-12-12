e6a2662401777a04ce78153d6c1e8f492dd5b343
K S Saivaibhav <saivaibhavks2022@gmail.com>
2024-12-12 12:17:59 +0530

    chnages

--- Arrays/remove-duplicates.js
+++ Arrays/remove-duplicates.js
@@ -2,6 +2,15 @@ const arr = [1, 2, 1, 2, 1, 3, 4, 3, 3, 4, 1];
 
 let temp = [];
 
-for (const item of arr) {
-  console.log("item", item);
+arr.sort((a, b) => a - b);
+
+temp.push(arr[0]);
+//temp ->1 ,1
+
+for (let i = 1; i < arr.length; i++) {
+  if (temp[arr.length - 1] !== arr[i]) {
+    temp.push(arr[i]);
+  }
 }
+
+console.log("temp", temp);
